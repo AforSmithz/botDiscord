6a7e87001ab23535f3346f0aaad239c735556bd1
AforSmithz <abiseno.arya@gmail.com>
2020-10-01 11:19:37 +0700

    adding comments

--- readLevel.cpp
+++ readLevel.cpp
@@ -2,17 +2,21 @@
 #include <cmath>
 #include <string>
 
-int checkLevel(std::string s);
+int checkLevel(std::string s); //prototyping function 
 
 int main()
 {
+    //initializing variables
     std::string s;
     int check; 
 
+    //prompt user for snippets and add it to the variable s
     std::cout << "enter snippets :" << std::endl; std::getline(std::cin, s);
     
+    //checking..
     check = checkLevel(s);
 
+    //mitigating edge cases for high number and very low number
     if(check < 1)
     {
         std::cout << "grade below 1" << std::endl;
@@ -28,30 +32,32 @@ int main()
 
 }
 
+//logic for checking reading level
 int checkLevel(std::string s)
 {
     int words = 1;
     int sentences = 0;
     int letters = 0;
 
-    for(int i = 0; i <= s.length() ; i++)
+    for(int i = 0; i <= s.length() ; i++) // looping through the snippets
     {
-        char character = s[i];
+        char character = s[i]; //adding the current character to a variable with type char
 
-        if(isalpha(character))
+        if(isalpha(character))//checking for letters
         {
             letters++;
         }
-        else if(isblank(character) && s[i+1] != ' ')
+        else if(isblank(character) && s[i+1] != ' ')//checking for words
         {
             words++;
         }
-        else if(character == '.' || character == '?'|| character == '!')
+        else if(character == '.' || character == '?'|| character == '!')//checking for sentences
         {
             sentences++;
         }
     }
 
+    //deciding the level with coleman-liau index
     float L = (letters /(float) words)* 100;
     float S = (sentences / (float) words) * 100;
 
